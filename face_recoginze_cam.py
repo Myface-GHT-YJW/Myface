3289c3431f5e6168542bf4278d4929033f55acb1
GHTlove <52912680+GHTlove@users.noreply.github.com>
2021-01-06 20:08:02 +0800

    Add files via upload

---
+++ face_recoginze_cam.py
@@ -0,0 +1,65 @@
+import face_recognition
+
+import cv2
+
+
+def FaceRecognize(img_name, tolerance=0.6):
+    cap = cv2.VideoCapture(0)
+
+    symbol_img = face_recognition.load_image_file(img_name)
+    symbol_face_encoding = face_recognition.face_encodings(symbol_img)[0]
+
+    face_locations = []
+    face_encodings = []
+    face_names = []
+    process_this_frame = True
+
+    while True:
+        ret, frame = cap.read()
+
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+        if process_this_frame:
+            face_locations = face_recognition.face_locations(small_frame)
+            face_encodings = face_recognition.face_encodings(small_frame, face_locations)
+
+            face_names = []
+            for face_encoding in face_encodings:
+                match = face_recognition.compare_faces([symbol_face_encoding], face_encoding, tolerance)
+
+                if match[0]:
+                    name = img_name.split('.')[0]
+                else:
+                    name = "unknown"
+
+                face_names.append(name)
+
+        process_this_frame = not process_this_frame
+
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), 2)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+        cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    img_path = "chy.jpg"
+    tolerance = 0.5
+    print(img_path)
+    print(tolerance)
+    FaceRecognize(img_path, tolerance)
